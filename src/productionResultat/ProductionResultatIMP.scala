76e014bc02f1bbb1309a22745d8a6c8e253fc290
yannb <yannb@Lenovo-Yann.home>
2022-02-05 11:42:45 +0100

    Correction bugs dans productionResultatIMP

--- src/productionResultat/ProductionResultatIMP.scala
+++ src/productionResultat/ProductionResultatIMP.scala
@@ -5,36 +5,36 @@ import library.Tag
 import library.Texte
 
 class ProductionResultatIMP extends ProductionResultat {
- 
+
   /**
-  *  @param l liste de couples solutions (titre, URL)
-  *  @return le document Html listant les solutions
-  */
- 
-  def reccResVersHtml(l: List[(String, String)]): Html = {
-      l match {
-        case (titre, url) :: rest => Tag("a", List(("href",url)), List(
-            Texte(titre),
-            reccResVersHtml(rest)))
-        case _ => Texte("")
-      }
+   *  @param l liste de couples solutions (titre, URL)
+   *  @return le document Html listant les solutions
+   */
+
+  def reccResVersHtml(l: List[(String, String)]): List[Html] = {
+    l match {
+      case (titre, url) :: Nil => Tag("a", List(("href", url)), List(Texte(titre))) :: Nil
+      case (titre, url) :: rest => Tag("a", List(("href", url)), List(
+        Texte(titre))) :: reccResVersHtml(rest)
+      case _ => Nil
     }
-  
-  /** 
+  }
+
+  /**
    *  A partir d'une liste de couples (titre, URL), produit un document Html, qui
    *  liste les solutions sous la forme de liens cliquables
-   *  
+   *
    *  @param l la liste des couples solutions (titre, URL)
    *  @return le document Html listant les solutions
    */
-  override def resultatVersHtml(l : List[(String,String)]): Html = {
-   l match {
-      case (titre, url) :: rest => Tag("html", List(),
-          List(
-            Tag("body", List(), List(
-              Tag("a", List(("href", url)), List(Texte(titre))),
-              reccResVersHtml(rest)))))
-      case _ => Texte("Aucun rÃ©sultat")
+  override def resultatVersHtml(l: List[(String, String)]): Html = {
+    l match {
+      case Nil => Tag("html", List(),
+        List(
+          Tag("body", List(), List(Texte("Aucun résultat")))))
+      case _ => Tag("html", List(),
+        List(
+          Tag("body", List(), reccResVersHtml(l))))
     }
   }
 }
